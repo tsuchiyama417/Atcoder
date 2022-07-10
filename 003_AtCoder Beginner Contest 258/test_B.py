1bbac89496a42620cf246d54b53500004d95ea03
{tsuchiyama} <{h22ms417@hirosaki-u.ac.jp}>
2022-07-10 16:36:59 +0900

    AtCoder

--- 003_AtCoder Beginner Contest 258/test_B.py
+++ 003_AtCoder Beginner Contest 258/test_B.py
@@ -1,10 +1,25 @@
-N = int(input())
+n = int(input())
+l = list()
 
-A = []
-for i in range(N):
-    num = int(input())
-    x = [int(a) for a in str(num)]
-    A.append(x)
+vector = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]
 
-print(A)
+max_val = -1
+for i in range(n):
+    s = input()
+    l.append(s)
 
+print(l)
+
+for i in range(n):
+    for j in range(n):
+        for direct in vector:
+            x, y = i, j
+            num = str()
+            for k in range(n):
+                x += direct[0]
+                y += direct[1]
+                num += l[x%n][y%n]
+                print(num)
+            num = int(num)
+            max_val = max(max_val, num)
+print(max_val)
